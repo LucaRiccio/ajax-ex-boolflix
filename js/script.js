bb7757ce574a8f0af2f66de84da3367c09b67f76
LucaRiccio <lucariccio888@gmail.com>
2020-09-04 16:15:36 +0200

    Milestone 3 (Aggiunta locandina film/serie)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// MILESTONE 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+//     Titolo
+//     Titolo Originale
+//     Lingua
+//     Voto
+
+// MILESTONE 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
+// MILESTONE 3:
+// In questa milestone come prima cosa aggiungiamo la copertina del film o della serie
+// al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo
+// perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+// Dovremo prendere quindi l’URL base delle immagini di TMDB:
+// https://image.tmdb.org/t/p/
+// per poi aggiungere la dimensione che vogliamo generare
+// (troviamo tutte le dimensioni possibili a questo link:
+// https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400
+// ) per poi aggiungere la
+// parte finale dell’URL passata dall’API.
+// Esempio di URL che torna la copertina di BORIS:
+// https://image.tmdb.org/t/p/w185/s2VDcsMh9ZhjFUxw77uCFDpTuXp.jpg
+
 $(document).ready(function(){
 
   $("#bottone-ricerca").click(function(){ // Al click del bottone..
@@ -81,7 +110,8 @@ function printFilm(data){
       title: data.results[i].title,
       original_title: data.results[i].original_title,
       language: flag(data.results[i].original_language),
-      vote_average: stars(data.results[i].vote_average) // Richiamata funzione per voto/stella
+      vote_average: stars(data.results[i].vote_average), // Richiamata funzione per voto/stella
+      poster_path: data.results[i].poster_path  // Aggiunta chiave per visualizzazione copertina.
     };
     var html = template(film);
     $(".stampa").append(html);
@@ -97,7 +127,8 @@ function printSerie(data){
       title: data.results[i].name,
       original_title: data.results[i].original_name,
       language: flag(data.results[i].original_language),
-      vote_average: stars(data.results[i].vote_average) // Richiamata funzione per voto/stella
+      vote_average: stars(data.results[i].vote_average), // Richiamata funzione per voto/stella
+      poster_path: data.results[i].poster_path
     };
     var html = template(serie);
     $(".stampa").append(html);
@@ -187,19 +218,3 @@ function flag(lingua){
 //   });
 //
 // });
-
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-//
-//     Titolo
-//     Titolo Originale
-//     Lingua
-//     Voto
-
-// Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
-// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
